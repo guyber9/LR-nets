6710a2e6667ec3a5ef8290016471a2d97879b8d7
guyber9 <guyberger9@gmail.com>
2021-06-23 10:06:20 +0300

    first version

--- layers.py
+++ layers.py
@@ -9,7 +9,6 @@ import numpy as np
 from torch.nn.modules.conv import _single, _pair, _triple, _reverse_repeat_tuple
 import numpy as np
 from torch.nn.common_types import _size_1_t, _size_2_t, _size_3_t
-import utils as utils
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.distributed as dist
 import os
