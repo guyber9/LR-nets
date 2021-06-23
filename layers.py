55886b9566ccdefe4f2b49e184d0859a30ce289a
guyber9 <guyberger9@gmail.com>
2021-06-23 16:15:37 +0300

    first version

--- layers.py
+++ layers.py
@@ -106,10 +106,12 @@ class LRnetConv2d(nn.Module):
                     my_array_2 = []
                     for idx in range(1, num_of_options + 1):
                         my_array_2.append([])
+                        print("my_array_2.append([" + str(idx) + ")])")
                     for n, theta in enumerate(val_2):
                         for idx in range(1, num_of_options + 1):
                             values_arr = np.random.default_rng().multinomial(1, theta)
                             values = np.nanargmax(values_arr) - 1
+                            print(idx)
                             my_array_2[idx].append(values)
                     for idx in range(1, num_of_options + 1):
                         my_array_1[idx].append(my_array_2[idx])
