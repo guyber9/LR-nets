83e1a72ee042e65651a40bc559084851f1114aba
guyber9 <guyberger9@gmail.com>
2021-07-02 16:06:57 +0300

    first version

--- models.py
+++ models.py
@@ -91,8 +91,11 @@ class LRNet_sign(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x)  # 32 x 24 x 24
+        print(x.size())
         x = self.conv2(x)  # 32 x 20 x 20
+        print(x.size())
         x = self.conv3(x)  # 64 x 16 x 16
+        print(x.size())
         x = F.relu(x)
         x = F.max_pool2d(x, 2)  # 64 x 8 x 8
         x = torch.flatten(x, 1)  # 1024
