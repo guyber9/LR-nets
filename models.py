51eca5091ca3d2476bdd26dbb1a6b8d30413c648
guyber9 <guyberger9@gmail.com>
2021-07-02 16:58:08 +0300

    first version

--- models.py
+++ models.py
@@ -64,7 +64,8 @@ class LRNet(nn.Module):
     def forward(self, x):
         x = self.conv1(x)  # 32 x 24 x 24
         hist1 = torch.histc(x, bins=100, min=-10, max=10)
-        print_full_tensor(hist1, "hist1")
+        torch.set_printoptions(threshold=10_000)
+        print(hist1)
         x = self.bn1(x)
         x = F.max_pool2d(x, 2) # 32 x 12 x 12
         x = F.relu(x)
