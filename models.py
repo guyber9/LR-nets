bee6a724ce1a0953cad4297e1fe4b030d88d7d97
guyber9 <guyberger9@gmail.com>
2021-07-06 08:28:46 +0300

    first version

--- models.py
+++ models.py
@@ -195,31 +195,35 @@ class LRNet_CIFAR10(nn.Module):
         self.dropout2 = nn.Dropout(0.5)
         self.fc1 = nn.Linear(8192, 1024)
         self.fc2 = nn.Linear(1024, 10)
-        self.dropout3 = nn.Dropout(0.2)
-        self.dropout4 = nn.Dropout(0.2)
+        self.dropout3 = nn.Dropout(0.15)
+        self.dropout4 = nn.Dropout(0.15)
+        self.dropout5 = nn.Dropout(0.15)
+        self.dropout6 = nn.Dropout(0.15)
 
     def forward(self, x):
         x = self.conv1(x)  # input is 3 x 32 x 32, output is 128 x 32 x 32
         # print("x1: " + str(x))
-        # x = self.bn1(x)  # <- problematic batchnoram (?)
+        x = self.bn1(x)  # <- problematic batchnoram (?)
         x = F.relu(x)
         # print("bn1: " + str(x))
+        x = self.dropout5(x)
         x = self.conv2(x)  # 128 x 32 x 32
         # print("x2: " + str(x))
         x = self.bn2(x)
         # print("bn2: " + str(x))
         x = F.max_pool2d(x, 2)  # 128 x 16 x 16
         x = F.relu(x)
-        # x = self.dropout3(x)
+        x = self.dropout3(x)
 
         x = self.conv3(x)  # 256 x 16 x 16
         x = self.bn3(x)
         x = F.relu(x)
+        x = self.dropout6(x)
         x = self.conv4(x)  # 256 x 16 x 16
         x = self.bn4(x)
         x = F.max_pool2d(x, 2)  # 256 x 8 x 8
         x = F.relu(x)
-        # x = self.dropout4(x)
+        x = self.dropout4(x)
 
         x = self.conv5(x)  # 512 x 8 x 8
         x = self.bn5(x)
@@ -230,10 +234,10 @@ class LRNet_CIFAR10(nn.Module):
         x = F.relu(x)
 
         x = torch.flatten(x, 1)  # 8192
-        # x = self.dropout1(x)
+        x = self.dropout1(x)
         x = self.fc1(x)  # 8192 -> 1024
         x = F.relu(x)
-        # x = self.dropout2(x)
+        x = self.dropout2(x)
         x = self.fc2(x)  # 1024 -> 10
         output = x
         # print("output: " + str(x))
