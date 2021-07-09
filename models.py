8aa4426a4b09b86ecd9643f101ca010c06d99a46
guyber9 <guyberger9@gmail.com>
2021-07-09 14:05:44 +0300

    first version

--- models.py
+++ models.py
@@ -101,11 +101,17 @@ class LRNet_sign(nn.Module):
         self.fc1 = nn.Linear(4096, 512)
         # self.fc1 = nn.Linear(6400, 512)
         self.fc2 = nn.Linear(512, 10)
+        self.bn1 = lrnet_nn.LRBatchNorm2d(32)
+        self.bn2 = lrnet_nn.LRBatchNorm2d(32)
+        self.bn3 = nn.BatchNorm2d(64)
 
     def forward(self, x):
         x = self.conv1(x)  # 32 x 24 x 24
+        x = self.bn1(x)
         x = self.conv2(x)  # 32 x 20 x 20
+        x = self.bn2(x)
         x = self.conv3(x)  # 64 x 16 x 16
+        x = self.bn3(x)
         x = F.relu(x)
         x = F.max_pool2d(x, 2)  # 64 x 8 x 8
         x = torch.flatten(x, 1)  # 1024
@@ -114,6 +120,7 @@ class LRNet_sign(nn.Module):
         x = self.fc2(x)
         output = x
         return output
+
 
 ##################
 ## CIFAR10 nets ##
@@ -242,16 +249,6 @@ class LRNet_CIFAR10(nn.Module):
         output = x
         # print("output: " + str(x))
         return output
-
-class LRNet_sign(nn.Module):
-    def __init__(self):
-        super(LRNet_sign, self).__init__()
-        self.conv1 = lrnet_nn.LRnetConv2d_not_sample(1, 32, 5, 1)
-        self.conv2 = lrnet_nn.NewLRnetConv2d(32, 32, 5, 1, output_sample=False)
-        self.conv3 = lrnet_nn.NewLRnetConv2d(32, 64, 5, 1, output_sample=True)
-        self.fc1 = nn.Linear(4096, 512)
-        # self.fc1 = nn.Linear(6400, 512)
-        self.fc2 = nn.Linear(512, 10)
 
 
 class LRNet_CIFAR10_ver2(nn.Module):
