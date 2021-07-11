09ce9617fa6e58b57581f533e823c0144b426866
guyber9 <guyberger9@gmail.com>
2021-07-11 09:27:57 +0300

    first version

--- models.py
+++ models.py
@@ -83,10 +83,10 @@ class LRNet(nn.Module):
         x = F.max_pool2d(x, 2) # 64 x 4 x 4
         x = F.relu(x)
         x = torch.flatten(x, 1) # 1024
-        # x = self.dropout1(x)
+        x = self.dropout1(x)
         x = self.fc1(x)
         x = F.relu(x)
-        # x = self.dropout2(x)
+        x = self.dropout2(x)
         x = self.fc2(x)
         output = x
         return output
@@ -232,24 +232,24 @@ class LRNet_CIFAR10(nn.Module):
         # x = self.bn1(x)  # <- problematic batchnoram (?)
         x = F.relu(x)
         # print("bn1: " + str(x))
-        x = self.dropout5(x)
+        # x = self.dropout5(x)
         x = self.conv2(x)  # 128 x 32 x 32
         # print("x2: " + str(x))
         x = self.bn2(x)
         # print("bn2: " + str(x))
         x = F.max_pool2d(x, 2)  # 128 x 16 x 16
         x = F.relu(x)
-        x = self.dropout3(x)
+        # x = self.dropout3(x)
 
         x = self.conv3(x)  # 256 x 16 x 16
         x = self.bn3(x)
         x = F.relu(x)
-        x = self.dropout6(x)
+        # x = self.dropout6(x)
         x = self.conv4(x)  # 256 x 16 x 16
         x = self.bn4(x)
         x = F.max_pool2d(x, 2)  # 256 x 8 x 8
         x = F.relu(x)
-        x = self.dropout4(x)
+        # x = self.dropout4(x)
 
         x = self.conv5(x)  # 512 x 8 x 8
         x = self.bn5(x)
@@ -260,10 +260,10 @@ class LRNet_CIFAR10(nn.Module):
         x = F.relu(x)
 
         x = torch.flatten(x, 1)  # 8192
-        x = self.dropout1(x)
+        # x = self.dropout1(x)
         x = self.fc1(x)  # 8192 -> 1024
         x = F.relu(x)
-        x = self.dropout2(x)
+        # x = self.dropout2(x)
         x = self.fc2(x)  # 1024 -> 10
         output = x
         # print("output: " + str(x))
