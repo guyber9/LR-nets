3364e329e9809e864a521908c0e3198cf7b8fc90
guyber9 <guyberger9@gmail.com>
2021-07-11 21:06:04 +0300

    first version

--- models.py
+++ models.py
@@ -245,10 +245,12 @@ class LRNet_CIFAR10(nn.Module):
         # print("x1: " + str(x))
         x = self.bn1(x)  # <- problematic batchnoram (?)
         print("bn1isnan: " + str(torch.isnan(x).any()))
+        print_full_tensor(x, "bn1 full")
         x = F.relu(x)
         # print("bn1: " + str(x))
         # x = self.dropout5(x)
         x = self.conv2(x)  # 128 x 32 x 32
+        print_full_tensor(x, "x2 full")
         print("x2isnan: " + str(torch.isnan(x).any()))
         print("x2: " + str(x))
         x = self.bn2(x)
@@ -256,17 +258,17 @@ class LRNet_CIFAR10(nn.Module):
         print("bn2: " + str(x))
         x = F.max_pool2d(x, 2)  # 128 x 16 x 16
         x = F.relu(x)
-        x = self.dropout3(x)
+        # x = self.dropout3(x)
 
         x = self.conv3(x)  # 256 x 16 x 16
         x = self.bn3(x)
         x = F.relu(x)
-        x = self.dropout6(x)
+        # x = self.dropout6(x)
         x = self.conv4(x)  # 256 x 16 x 16
         x = self.bn4(x)
         x = F.max_pool2d(x, 2)  # 256 x 8 x 8
         x = F.relu(x)
-        x = self.dropout4(x)
+        # x = self.dropout4(x)
 
         x = self.conv5(x)  # 512 x 8 x 8
         x = self.bn5(x)
