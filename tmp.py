45b1caa15d1328ba03501f3776ad83c659c1d948
guyber9 <guyberger9@gmail.com>
2021-07-14 18:40:33 +0300

    first version

--- tmp.py
+++ tmp.py
@@ -7,9 +7,8 @@ w = loaded['w']
 
 z = F.conv2d(x, w, None, 1, 1, 1, 1)
 v = torch.sqrt(z)
-print("v isnan: " + str(torch.isnan(v).any()))
 print("v: " + str(v))
-print("v: " + str(v))
+print("z: " + str(z))
 
 print("x: " + str(x))
 print("x < 0: " + str(x < 0))
@@ -21,3 +20,4 @@ print("x is positive: " + str((x < 0).any()))
 print("w is positive: " + str((w < 0).any()))
 print("z is positive: " + str((z < 0).any()))
 print("v is positive: " + str((v < 0).any()))
+print("v isnan: " + str(torch.isnan(v).any()))
