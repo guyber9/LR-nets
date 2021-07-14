6d60c057495e680527f7022964636a6c0d061ca2
guyber9 <guyberger9@gmail.com>
2021-07-14 23:24:41 +0300

    first version

--- tmp.py
+++ tmp.py
@@ -1,6 +1,6 @@
 import torch
 from torch.nn import functional as F
-
+from utils import print_fullllll_tensor
 # loaded = torch.load('my_tensors.pt')
 # print(loaded)
 # x = loaded['x']
@@ -15,14 +15,16 @@ v = torch.sqrt(z)
 print("v: " + str(v))
 print("z: " + str(z))
 
+print_fullllll_tensor(z, "z")
+
 print("x: " + str(x))
 print("x < 0: " + str(x < 0))
 
 print("w: " + str(w))
 print("w < 0: " + str(w < 0))
 
-print("x is positive: " + str((x < 0).any()))
-print("w is positive: " + str((w < 0).any()))
-print("z is positive: " + str((z < 0).any()))
-print("v is positive: " + str((v < 0).any()))
+print("x is negative: " + str((x < 0).any()))
+print("w is negative: " + str((w < 0).any()))
+print("z (= Wx) is negative: " + str((z < 0).any()))
+print("v is negative: " + str((v < 0).any()))
 print("v isnan: " + str(torch.isnan(v).any()))
