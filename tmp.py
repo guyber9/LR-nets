28be1d50332dd6774b90e7ccbe23bef2a2f0d3c6
guyber9 <guyberger9@gmail.com>
2021-07-15 00:32:33 +0300

    first version

--- tmp.py
+++ tmp.py
@@ -4,17 +4,8 @@ from torch.nn import functional as F
 loaded = torch.load('my_tensors.pt')
 x = loaded['x']
 w = loaded['w']
-# loaded1 = torch.load('my_tensors1.pt')
-# loaded2 = torch.load('my_tensors2.pt')
-x = loaded['x']
-w = loaded['w']
-
 z = F.conv2d(x, w, None, 1, 1, 1, 1)
 v = torch.sqrt(z)
-
-print("x: " + str(x))
-print("w: " + str(w))
-
 print("x is negative: " + str((x < 0).any()))
 print("w is negative: " + str((w < 0).any()))
 print("z (= Wx) is negative: " + str((z < 0).any()))
