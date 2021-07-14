ecc0b791586bef1e107cee7756cdd4e4417000d1
guyber9 <guyberger9@gmail.com>
2021-07-14 18:25:45 +0300

    first version

--- tmp.py
+++ tmp.py
@@ -1,7 +1,6 @@
 import torch
 from torch.nn import functional as F
 
-
 loaded = torch.load('my_tensors.pt')
 x = loaded['x']
 w = loaded['w']
@@ -9,3 +8,5 @@ w = loaded['w']
 z1 = F.conv2d(x, w, None, 1, 1, 1, 1)
 v = torch.sqrt(z1)
 print("v isnan: " + str(torch.isnan(v).any()))
+print("x is positive: " + str((x > 0).any()))
+print("x > 0: " + str(x > 0))
