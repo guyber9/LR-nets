b6e6d423a8a0eed9b71a16007821a49bf6c1c186
guyber9 <guyberger9@gmail.com>
2021-07-15 00:31:32 +0300

    first version

--- tmp.py
+++ tmp.py
@@ -12,6 +12,9 @@ w = loaded['w']
 z = F.conv2d(x, w, None, 1, 1, 1, 1)
 v = torch.sqrt(z)
 
+print("x: " + str(x))
+print("w: " + str(w))
+
 print("x is negative: " + str((x < 0).any()))
 print("w is negative: " + str((w < 0).any()))
 print("z (= Wx) is negative: " + str((z < 0).any()))
