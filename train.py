abc109e84639f291b5155da63702ce3e6fe9ae99
guyber9 <guyberger9@gmail.com>
2021-07-17 22:29:47 +0300

    first version

--- train.py
+++ train.py
@@ -254,13 +254,23 @@ def main_train():
         elif args.cifar10:
             if args.ver2:
                 # TODO
-                optimizer = optim.Adam(net.parameters(), lr=args.lr, weight_decay=weight_decay)
-                # optimizer = optim.Adam([
-                #     {'params': net.conv1.parameters(), 'weight_decay': probability_decay},
-                #     {'params': net.conv2.parameters(), 'weight_decay': probability_decay},
-                #     {'params': net.fc1.parameters(), 'weight_decay': weight_decay},
-                #     {'params': net.fc2.parameters(), 'weight_decay': weight_decay}
-                # ], lr=args.lr, weight_decay=weight_decay)
+                # optimizer = optim.Adam(net.parameters(), lr=args.lr, weight_decay=weight_decay)
+                optimizer = optim.Adam([
+                    {'params': net.conv1.parameters(), 'weight_decay': probability_decay},
+                    {'params': net.conv2.parameters(), 'weight_decay': probability_decay},
+                    {'params': net.conv3.parameters(), 'weight_decay': probability_decay},
+                    {'params': net.conv4.parameters(), 'weight_decay': probability_decay},
+                    {'params': net.conv5.parameters(), 'weight_decay': probability_decay},
+                    {'params': net.conv6.parameters(), 'weight_decay': probability_decay},
+                    {'params': net.fc1.parameters()},
+                    {'params': net.fc2.parameters()},
+                    {'params': net.bn1.parameters()},
+                    {'params': net.bn2.parameters()},
+                    {'params': net.bn3.parameters()},
+                    {'params': net.bn4.parameters()},
+                    {'params': net.bn5.parameters()},
+                    {'params': net.bn6.parameters()}
+                ], lr=args.lr, weight_decay=weight_decay)
             else:
                 optimizer = optim.Adam([
                     {'params': net.conv1.parameters(), 'weight_decay': probability_decay},
