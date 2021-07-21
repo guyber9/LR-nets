ae2cd5efbcdd55847e8a8d2a70213159f8df0bb2
guyber9 <guyberger9@gmail.com>
2021-07-21 18:46:01 +0300

    first version

--- train.py
+++ train.py
@@ -374,8 +374,8 @@ def main_train():
 
         net.test_mode_switch(1, args.tickets)
         net.collect_stats_switch_on() # TODO morning
-        # if epoch > 20:
-        net.update_use_test_stats(True)
+        if epoch > 100:
+            net.update_use_test_stats(True)
         _, __, ___ = test(net, criterion, epoch, device, testloader, args, best_acc, best_epoch, test_mode=True, f=f, eval_mode=True) # note: model is saved only in above test method
         scheduler.step()
 
