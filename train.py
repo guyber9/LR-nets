71da4a56a3c3fed05a9da90e89acc187afe14ecd
guyber9 <guyberger9@gmail.com>
2021-07-21 19:18:26 +0300

    first version

--- train.py
+++ train.py
@@ -377,6 +377,10 @@ def main_train():
         if epoch > 100:
             net.update_use_test_stats(True)
         _, __, ___ = test(net, criterion, epoch, device, testloader, args, best_acc, best_epoch, test_mode=True, f=f, eval_mode=True) # note: model is saved only in above test method
+        print("net.bn1.test_running_mean: " + str(net.bn1.test_running_mean))
+        print("net.bn1.test_running_var: " + str(net.bn1.test_running_var))
+        print("net.bn2.test_running_mean: " + str(net.bn2.test_running_mean))
+        print("net.bn2.test_running_var: " + str(net.bn2.test_running_var))
         scheduler.step()
 
         if args.collect_stats:
