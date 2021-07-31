1bfdfce97f592892744221d6771fcf4a08b27761
guyber9 <guyberger9@gmail.com>
2021-07-31 18:03:02 +0300

    update

--- models.py
+++ models.py
@@ -137,15 +137,16 @@ class LRNet_ver2(nn.Module):
     def forward(self, x):
         assertnan(x, "x0")
         x = self.conv1(x)  # 32 x 24 x 24
-        m,v = x
+        # m,v = x
         # print("m1: ", m)
         # print("v1: ", v)
-        assertnan(m, "m1")
-        assertnan(v, "v1")
+        # assertnan(m, "m1")
+        # assertnan(v, "v1")
+        assertnan(x, "x1")
         x = self.bn1(x)
-        m,v = x
-        assertnan(m, "mbn1")
-        assertnan(v, "vbn1")
+        # m,v = x
+        # assertnan(m, "mbn1")
+        # assertnan(v, "vbn1")
         # x = self.conv2(x)  # 32 x 20 x 20
         # m,v = x
         # print("m2: ", m)
